49613477731c65b24ff4df473e6c64ae6809c92d
heidar <heidar.ahmadi89@gmail.com>
2022-11-13 00:29:44 +0330

    Add an abstraction for register services

---
+++ NikAmooz.UrlShortener.Api/Installer/MvcInstaller.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using NikAmooz.UrlShortener.Api.Middlewares;
+
+namespace NikAmooz.UrlShortener.Api.Installer
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddControllers(opt => opt.Filters.Add<OnExceptionMiddleware>())
+               .AddFluentValidation();
+
+            #region Swagger
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NikAmooz Url Shortener API"
+                });
+            });
+
+            #endregion Swagger
+
+        }
+    }
+}
