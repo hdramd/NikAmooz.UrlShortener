1bab8048097ef81b4cc4fb9d4176c243d22a351e
heidar <heidar.ahmadi89@gmail.com>
2022-11-12 22:28:56 +0330

    Install ef core package and add IAppDbContext to application layer

--- NikAmooz.UrlShortener.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NikAmooz.UrlShortener.Application
-{
-    public class Class1
-    {
-    }
-}
---
+++ NikAmooz.UrlShortener.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NikAmooz.UrlShortener.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NikAmooz.UrlShortener.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        #region DbSets
+
+        DbSet<ShortUrl> ShortUrls { get; } 
+
+        #endregion
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
