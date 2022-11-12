d7b91bd28613de9513cd58c27ea74033911d2743
heidar <heidar.ahmadi89@gmail.com>
2022-11-12 23:43:55 +0330

    Register application services

---
+++ NikAmooz.UrlShortener.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NikAmooz.UrlShortener.Application.Common.Behaviours;
+
+namespace NikAmooz.UrlShortener.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+
+            return services;
+        }
+    }
+}
